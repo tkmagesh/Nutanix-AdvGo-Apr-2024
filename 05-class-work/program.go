4d9d7dc93e3f87a008376fa7a1278bca78bd0a76
Magesh Kuppan <tkmagesh77@gmail.com>
2024-04-02 15:16:43 +0530

    updates

--- 05-class-work/program.go
+++ 05-class-work/program.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
 /*
@@ -57,8 +59,65 @@ func (product Product) String() string {
 	return fmt.Sprintf("Id = %d, Name = %q, Cost = %.2f, Units = %d, Category = %q", product.Id, product.Name, product.Cost, product.Units, product.Category)
 }
 
+var ErrProductNotFound error = errors.New("product not found")
+
+type Products []Product
+
+func (products Products) String() string {
+	var sb strings.Builder
+	for _, p := range products {
+		sb.WriteString(fmt.Sprintf("%s\n", p))
+	}
+	return sb.String()
+}
+
+func (products Products) IndexOf(p Product) (int, error) {
+	for idx, prod := range products {
+		if p == prod {
+			return idx, nil
+		}
+	}
+	return 0, ErrProductNotFound
+}
+
+func (products Products) Includes(p Product) bool {
+	_, err := products.IndexOf(p)
+	return err == nil
+}
+
+/*
+func (products Products) FilterCostlyProducts() Products {
+	var result Products
+	for _, p := range products {
+		if p.Cost > 1000 {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+
+func (products Products) FilterStationaryProducts() Products {
+	var result Products
+	for _, p := range products {
+		if p.Category == "Stationary" {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+*/
+
+func (products Products) Filter(predicate func(Product) bool) Products {
+	var result Products
+	for _, p := range products {
+		if predicate(p) {
+			result = append(result, p)
+		}
+	}
+	return result
+}
 func main() {
-	products = []Product{
+	products := Products{
 		Product{105, "Pen", 5, 50, "Stationary"},
 		Product{107, "Pencil", 2, 100, "Stationary"},
 		Product{103, "Marker", 50, 20, "Utencil"},
@@ -67,6 +126,35 @@ func main() {
 		Product{104, "Scribble Pad", 20, 20, "Stationary"},
 		Product{109, "Golden Pen", 2000, 20, "Stationary"},
 	}
+	fmt.Println("Initial List")
+	fmt.Println(products)
 
-	//
+	// IndexOf
+	fmt.Println("IndexOf")
+	stove := Product{102, "Stove", 5000, 5, "Utencil"}
+	if idx, err := products.IndexOf(stove); err == nil {
+		fmt.Println("Index Of Stove : ", idx)
+	} else {
+		fmt.Println("Stove not found")
+	}
+
+	// Includes
+	fmt.Println("Is stove in the product list ?:", products.Includes(stove))
+
+	// filter costly products
+	fmt.Println("Costly Products")
+	// costlyProducts := products.FilterCostlyProducts()
+	costlyProductPredicate := func(p Product) bool {
+		return p.Cost > 1000
+	}
+	costlyProducts := products.Filter(costlyProductPredicate)
+	fmt.Println(costlyProducts)
+
+	fmt.Println("Stationary Products")
+	// stationaryProducts := products.FilterStationaryProducts()
+	stationaryProductPredicate := func(p Product) bool {
+		return p.Category == "Stationary"
+	}
+	stationaryProducts := products.Filter(stationaryProductPredicate)
+	fmt.Println(stationaryProducts)
 }
